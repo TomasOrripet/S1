941a405888614099ad4b841eeca243172f9cec24
TomasOrripet <70690540+TomasOrripet@users.noreply.github.com>
2021-09-10 12:39:55 +0000

    Update Percolation.java

--- Code/src/Percolation.java
+++ Code/src/Percolation.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
@@ -18,27 +17,14 @@ public class Percolation {
         grid = new boolean[n][n];
     }
 
-    public boolean percolates() {
-        if (unions.connected(0, 101)) {
-            return true;
-        }
-        return false;
-    }
-
     public void open(int row, int col) {
         if (isOpen(row, col)) {
             return;
         }
-        StdOut.print(row);
-        StdOut.print(" " + col + "\n");
+        boundaries(row, col);
         open_count++;
         grid[row][col] = true;
         connect(row, col);
-    }
-
-    public String numberOfOpenSites() {
-
-        return String.valueOf(open_count);
     }
 
     public boolean isOpen(int row, int col) {
@@ -53,6 +39,22 @@ public class Percolation {
         return false;
     }
 
+    public String numberOfOpenSites() {
+        return String.valueOf(open_count);
+    }
+
+    public boolean percolates() {
+        if (unions.connected(0, 101)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
     private void boundaries(int row, int col) {
         if (row >= size || row < 0 || col >= size || col < 0) {
             throw new java.lang.IndexOutOfBoundsException("Index out of bounds");
@@ -61,28 +63,20 @@ public class Percolation {
 
     private void connect(int row, int col) {
         int tile = size * row + col + 1;
-        StdOut.print(tile);
         if (row != 0 && isOpen(row - 1, col)) {
             unions.union(((size * (row - 1) + col) + 1), tile);
-            StdOut.print("down\n");
         }
         if (row != (size - 1) && isOpen(row + 1, col)) {
             unions.union(tile, ((size * (row + 1) + col) + 1));
-            StdOut.print("up" + (size * row + 1 + col) + "\n");
         }
         if (col != 0 && isOpen(row, col - 1)) {
             unions.union((size * row + col), tile);
-            StdOut.print("left\n");
         }
         if (col != (size - 1) && isOpen(row, col + 1)) {
             unions.union(tile, ((size * row + col + 2)));
-            StdOut.print("right: " + (size * row + col + 1) + "\n");
         }
     }
 
 
-    public static void main(String[] args) {
-
-    }
 }
 ;
