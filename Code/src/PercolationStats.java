08bef6bf6bec9e3505660a07cad5bcd1b5d8d5b0
TomasOrripet <70690540+TomasOrripet@users.noreply.github.com>
2021-09-10 15:07:08 +0000

    Add files via upload

---
+++ Code/src/PercolationStats.java
@@ -0,0 +1,54 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class PercolationStats {
+    private double[] times;
+
+    public PercolationStats(int N, int T) // perform T independent experiments on an N-by-N grid
+    {
+        times = new double[T];
+        for (int i = 0; i < T; i++) {
+            Stopwatch timer = new Stopwatch();
+            Percolation percolation = new Percolation(N);
+            do {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                percolation.open(row, col);
+            } while (!percolation.percolates());
+            double time = timer.elapsedTime();
+            times[i] = time;
+
+        }
+        StdOut.print("mean: " + mean() + "\n");
+        StdOut.print("Stddev: " + stddev() + "\n");
+        StdOut.print("conf low: " + confidenceLow() + "\n");
+        StdOut.print("conf high: " + confidenceHigh() + "\n");
+    }
+
+    public double mean() // sample mean of percolation threshold
+    {
+        return StdStats.mean(times);
+    }
+
+    public double stddev() // sample standard deviation of percolation threshold
+    {
+        return StdStats.stddev(times);
+    }
+
+    public double confidenceLow() // low endpoint of 95% confidence interval
+    {
+
+        return mean() - (1.96 * stddev() / Math.sqrt(times.length));
+    }
+
+    public double confidenceHigh() // high endpoint of 95 confidence interval
+    {
+        return mean() + (1.96 * stddev() / Math.sqrt(times.length));
+    }
+
+    public static void main(String[] args) {
+        new PercolationStats(1024, 10);
+    }
+}
